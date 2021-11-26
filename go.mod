1b19f0891de9a96bcbad96950488f9929acf0c9f
Aleks Fazlieva <aleks.fazlieva@soundcloud.com>
2021-11-26 10:10:58 +0100

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/britishrum/test-renovated-repo-dep
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,6 @@
+package versions
+
+// Returns version of this module
+func GetVersion() string {
+	return "v0.0.1"
+}
